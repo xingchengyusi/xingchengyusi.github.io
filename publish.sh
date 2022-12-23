66876910b8873eac37f4bd9b8aace0ea24e37360
Chengzhi <zsxxxzks1314@gmail.com>
2022-12-23 14:14:32 -0800

    hexo backup

--- publish.sh
+++ publish.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# hexo deploy
+hexo g
+hexo clean
+hexo deploy
+echo "success deploy"
+
+# github backup
 git remote add origin git@github.com:xingchengyusi/xingchengyusi.github.io.git
 git add .
 git commit -m "hexo backup"
 git push origin backup
+echo "success backup"
